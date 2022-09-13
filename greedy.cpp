7ee80c2535c62c6901a1ab9da091817ba60407c1
Matheus Buratti <matheusburatti00@gmail.com>
2022-09-12 23:43:43 -0300

    search greedy

--- greedy.cpp
+++ greedy.cpp
@@ -25,21 +25,23 @@ std::vector<int> fastGreedySolver(Problem P){
 }
 
 std::vector<int> searchGreedySolver(Problem P){
-    std::list<int> Items(P.items.begin(),P.items.end());
-    std::vector<int> bins;
+    std::list<int> Items(P.items.begin(),P.items.end()); // Copia do vetor de items em uma lista
+    std::vector<int> bins; // Vetor de bins
     while(Items.size() > 0){
         bins.push_back(*Items.begin()); // "Cria" uma nova bin no fim do vetor e adiciona o primeiro item da lista
         int freeSpace = P.binCapacity - *Items.begin(); // Resto de espaço na bin depois de adicionar o primeiro item
-        Items.erase(Items.begin()); // Remove o primeiro item
+        Items.pop_front(); // Remove o primeiro item
 
-        std::list<int>::iterator it = std::upper_bound(Items.begin(), Items.end(), freeSpace, std::greater<int>());
-        // Procura o próximo item com tamanho <= ao espaço sobrando na bin
+        std::list<int>::iterator it; // Próximo item com tamanho <= ao espaço sobrando na bin
+        it = std::upper_bound(Items.begin(), Items.end(), freeSpace, std::greater<int>());
+        // Upper_bound é uma busca sequencial e não binária nesse caso por causa da estrutura escolhida ser do 
+        // tipo list, porém essa desvantagem é compensada por causa das remoções dos items ser O(1).
         
         while(*it <= freeSpace && it != Items.end()){
-            *(bins.end()-1) += *it;
+            *(bins.end()-1) += *it; // Adiciona à última bin o maior valor encontrado que cabe.
             freeSpace -= *it;
-            it = Items.erase(it);
-            if(it != Items.end())
+            it = Items.erase(it); // Remove da lista o item e retorna o próximo
+            if(it != Items.end()) // se o próximo não for o fim, busca por mais para continuar preenchendo o espaço restante
                 it = std::upper_bound(it, Items.end(), freeSpace, std::greater<int>());
         }
     }
